3295430561009541d38ffd41884b3c60ee41d4bc
SwaggyP <1352164869@qq.com>
2022-05-28 09:21:22 +0000

    Update router.go

--- router.go
+++ router.go
@@ -12,10 +12,21 @@ func initRouter(r *gin.Engine) {
 	apiRouter := r.Group("/douyin")
 
 	// basic apis
-	apiRouter.GET("/feed/", controller.Feed)
+	apiRouter.GET("/feed/", controller.Feed) // PYH
 	//apiRouter.GET("/user/", controller.UserInfo)
-	apiRouter.POST("/user/register/", controller.Register)
-	apiRouter.POST("/user/login/", controller.Login) //testhere
+	apiRouter.POST("/user/register/", controller.Register) // PYH
+	apiRouter.POST("/user/login/", controller.Login) // CMD
 	//apiRouter.POST("/publish/action/", controller.Publish)
-	apiRouter.GET("/publish/list/", controller.PublishList)
+	apiRouter.GET("/publish/list/", controller.PublishList) // PYH
+
+	// extra apis - I
+	//apiRouter.POST("/favorite/action/", controller.FavoriteAction)
+	//apiRouter.GET("/favorite/list/", controller.FavoriteList)
+	//apiRouter.POST("/comment/action/", controller.CommentAction)
+	//apiRouter.GET("/comment/list/", controller.CommentList)
+
+	// extra apis - II
+	//apiRouter.POST("/relation/action/", controller.RelationAction)
+	//apiRouter.GET("/relation/follow/list/", controller.FollowList)
+	//apiRouter.GET("/relation/follower/list/", controller.FollowerList)
 }
