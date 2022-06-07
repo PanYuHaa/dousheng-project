657e5bc61d0032a28773b1a2fe00f0a95948dd26
IanChen0713 <18565380707@163.com>
2022-06-07 23:35:18 +0800

    针对服务器修改了些参数

--- service/publish_video.go
+++ service/publish_video.go
@@ -10,13 +10,13 @@ import (
 
 func PublishVideo(user model.User, finalName string, title string) error {
 	// playUrl := "http://[$主机ip]:8080/static/video/" + finalName
-	playUrl := "http://192.168.10.103:8080/static/video/" + finalName
+	playUrl := "http://124.223.184.55:8080/static/video/" + finalName
 	err := getSnapshot(finalName)
 	if err != nil {
 		return err
 	}
 	// coverUrl := "http://[$主机ip]:8080/static/video/" + finalName
-	coverUrl := "http://192.168.10.103:8080/static/cover/" + getPicName(finalName)
+	coverUrl := "http://124.223.184.55:8080/static/cover/" + getPicName(finalName)
 	return repository.AddVideo(model.Video{
 		Id: repository.VideoAmount() + 1,
 		Author: model.User{
@@ -35,7 +35,7 @@ func getSnapshot(finalName string) error {
 	inputPath := "./public/video/" + finalName
 	outputPath := "./public/cover/" + outPicName
 	// 调用ffmpeg应用程序进行视频截图
-	cmd := exec.Command("./ffmpeg", "-i", inputPath, "-ss", "1", "-f", "image2", "-frames:v", "1", outputPath)
+	cmd := exec.Command("ffmpeg", "-i", inputPath, "-ss", "1", "-f", "image2", "-frames:v", "1", outputPath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	// 由于有些ffmpeg的bug没有解决所以暂时不反回error
